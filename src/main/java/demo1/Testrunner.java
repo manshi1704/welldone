348cdbf52ae500e883a26e05118e06c4546ebbe0
manshi1704 <60376673+manshi1704@users.noreply.github.com>
2020-01-29 13:44:42 +0530

    commit3

--- src/main/java/demo1/Testrunner.java
+++ src/main/java/demo1/Testrunner.java
@@ -9,8 +9,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin={"json:target/cucumber.json"},
-		features="Feature12"
-		,glue= {"step_Definition"})
+		//features="Feature12"
+		//,glue= {"step_Definition"})
 
 public class Testrunner {
 
