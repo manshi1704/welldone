0a1dc2c0af56105808b7eacf24c4d8d1f7c3e313
manshi1704 <60376673+manshi1704@users.noreply.github.com>
2020-01-29 13:30:47 +0530

    assert

--- src/main/java/step_Definition/step_def.java
+++ src/main/java/step_Definition/step_def.java
@@ -1,6 +1,6 @@
 package step_Definition;
 
-import org.junit.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -41,14 +41,14 @@ public class step_def {
 	driver.findElement(By.name("password")).click();
 	driver.findElement(By.name("password")).sendKeys(password);
 	driver.findElement(By.name("Login")).click();
-//	driver.findElement(By.linkText(" SignOut")).getText();
+	driver.findElement(By.linkText(" SignOut")).getText();
 	//Assert.assertEquals(" Sign Out", "driver.findElement(By.linkText(\" SignOut\")).getText()");
 	}
 
 
 	@Then("Message displayed Login Successfully")
 	public void message_displayed_Login_Successfully() {
-		Assert.assertEquals(" Sign Out", "driver.findElement(By.linkText(\" SignOut\")).getText()");
+		
 	}
 
 
